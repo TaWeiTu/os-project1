09e90cc681b12254b9fc7d821af61150b0ca0069
TaWeiTu <tu.da.wei@gmail.com>
2020-04-11 03:50:03 +0800

    Add system call

--- src/job.c
+++ src/job.c
@@ -1,26 +1,26 @@
-#define _GNU_SOURCE
-
-#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/syscall.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 #include "unit.h"
+
+#define GET_TIME 335
+#define PRINT_TIME 336
 
 int main(int argc, const char **argv) {
   if (argc != 3) {
     fprintf(stderr, "[Usage] ./job process-name execution-time\n");
     exit(1);
   }
-  struct timeval st, ed;
-  gettimeofday(&st, NULL);
   printf("%s %d\n", argv[1], (int)getpid());
+  struct timespec st, ed;
+  syscall(GET_TIME, &st);
   unsigned exec_time = (unsigned)atoi(argv[2]);
   Sleep(exec_time);
-  gettimeofday(&ed, NULL);
-  fprintf(stderr, "[Project 1] %d %ld.%06ld %ld.%06ld\n", (int)getpid(),
-          st.tv_sec, st.tv_usec, ed.tv_sec, ed.tv_usec);
+  syscall(GET_TIME, &ed);
+  syscall(PRINT_TIME, getpid(), &st, &ed);
   return 0;
 }
